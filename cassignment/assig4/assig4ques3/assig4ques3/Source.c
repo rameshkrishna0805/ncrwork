9409520dcc3d05e46597a21840d91a3c63e0cf7f
cdac <cdac@DESKTOP-SS5H7HT>
2019-02-07 00:17:36 -0800

    question4

---
+++ cassignment/assig4/assig4ques3/assig4ques3/Source.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	char *s[] = { "we will teach you how to","move a mountain","level a building","erase the past","make a million" };
+	for (int i = 0;i < 5;i++)
+	{
+		for (int j = 0;j < (strlen(s[i]) - 1);j++)
+		{
+			if(s[i][j]=='e')
+			count++;
+		}
+	}
+	printf("%d", count);
+	getch();
+	return 0;
+}
