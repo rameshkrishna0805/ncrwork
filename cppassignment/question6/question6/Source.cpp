41819b78248de8fe70ce91b64b82253c2fa3c295
rameshkrishna0805 <vijayarameshkrishna@gmail.com>
2019-02-14 01:53:36 -0800

    question6-->sqrt

---
+++ cppassignment/question6/question6/Source.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+class point {
+	int x;
+	int y;
+public:
+	void read(int a, int b)
+	{
+		x = a;
+		y = b;
+
+	}
+	friend void add(point a, point b);
+};
+int main()
+{
+	point p, q;
+	p.read(1, 1);
+	q.read(3, 3);
+	add(p, q);
+	getchar();
+	return 0;
+}
+
+void add(point a, point b)
+
+{
+	cout << sqrt(pow((b.x - a.x), 2) + pow((b.y - a.y), 2));
+}
