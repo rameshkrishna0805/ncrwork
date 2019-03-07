fe71dd5ed693f3bbb6e99e0c333460a429c7715c
rameshkrishna0805 <vijayarameshkrishna@gmail.com>
2019-03-06 21:02:44 -0800

    question2

---
+++ ds/windows/question2/question2/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+int main()
+{
+	LPWSTR *CommandLineArguments;
+	int NoofArgs;
+	CommandLineArguments = CommandLineToArgvW(GetCommandLineW(), &NoofArgs);
+	if (CommandLineArguments == NULL)
+		cout << "Failed" << endl;
+	else
+	{
+		cout << "The Command Line Arguments are - " << endl;
+		for (int i = 0; i < NoofArgs; i++)
+		{
+			wcout << CommandLineArguments[i] << endl;
+		}
+	}
+	system("pause");
+}
